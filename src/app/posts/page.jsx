09230e15236bf5d861aa8566746c75d89bbec8c7
Vishal Ludhrani <vludrani@gmail.com>
2024-08-07 17:24:09 +0530

    chore: test netlify form submission

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -26,6 +26,32 @@ export default async function BlogLanding() {
           </Link>
         ))}
       </section>
+      <form method="POST" data-netlify="true">
+        <ul className="flex flex-col sm:flex-row">
+          <li className="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
+            <div className="relative flex items-start w-full">
+              <div className="flex items-center h-5">
+                <input id="like" name="like-dislike" type="radio" className="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" />
+              </div>
+              <label htmlFor="like" className="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
+                Like
+              </label>
+            </div>
+          </li>
+
+          <li className="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium bg-white border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg sm:-ms-px sm:mt-0 sm:first:rounded-se-none sm:first:rounded-es-lg sm:last:rounded-es-none sm:last:rounded-se-lg dark:bg-neutral-800 dark:border-neutral-700 dark:text-white">
+            <div className="relative flex items-start w-full">
+              <div className="flex items-center h-5">
+                <input id="dislike" name="like-dislike" type="radio" className="border-gray-200 rounded-full disabled:opacity-50 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" />
+              </div>
+              <label htmlFor="dislike" className="ms-3 block w-full text-sm text-gray-600 dark:text-neutral-500">
+                Dislike
+              </label>
+            </div>
+          </li>
+        </ul>
+        <button type="submit" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">Submit</button>
+      </form>
     </main>
   )
 }
