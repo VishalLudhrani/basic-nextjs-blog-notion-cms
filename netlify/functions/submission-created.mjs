a05c41d2e09dbeb8f5a42024c79dbdaeb171ef1c
Vishal Ludhrani <vludrani@gmail.com>
2024-08-08 15:48:48 +0530

    chore: netlify form function route params config

--- netlify/functions/submission-created.mjs
+++ netlify/functions/submission-created.mjs
@@ -1,4 +1,9 @@
-export default async function helloWorld(req, context) {
-  console.log("Submission created\n-----req-----" + JSON.stringify(req) + "\n-----context-----\n" + JSON.stringify(context))
+export default async (req, context) => {
+  console.log("\nSubmission created\n-----req-----\n" + JSON.stringify(req) + "\n-----context-----\n" + JSON.stringify(context))
+  console.log("\nparams\n", JSON.stringify(context.params))
   return new Response("Submission created\n-----req-----" + JSON.stringify(req) + "\n-----context-----\n" + JSON.stringify(context))
+}
+
+export const config = {
+  path: ["posts", "posts/:slug"]
 }
