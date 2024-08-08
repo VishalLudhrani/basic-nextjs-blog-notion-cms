9054748219e76e54882ecba8b15eac9e14e80813
Vishal Ludhrani <vludrani@gmail.com>
2024-08-08 15:15:24 +0530

    chore: inspect netlify function execution with console statement

--- netlify/functions/submission-created.mjs
+++ netlify/functions/submission-created.mjs
@@ -1,3 +1,4 @@
 export default async function helloWorld(req, context) {
+  console.log("Submission created\n-----req-----" + JSON.stringify(req) + "\n-----context-----\n" + JSON.stringify(context))
   return new Response("Submission created\n-----req-----" + JSON.stringify(req) + "\n-----context-----\n" + JSON.stringify(context))
 }
