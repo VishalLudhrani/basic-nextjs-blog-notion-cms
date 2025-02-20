7ff9b588233fb76728fbdef229860cc4115be9da
Vishal Ludhrani <vludrani@gmail.com>
2025-02-20 13:38:14 +0530

    chore: configure algolia search

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`dark ${inter.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+        <Script src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js" />
+      </head>
+      <body className={`dark ${inter.className}`}>
+        {children}
+        <Script>
+          {
+            `
+              window.algoliasearchNetlify({
+                appId: 'WWO4R9FSTI',
+                apiKey: 'f7b98a870a6130f09275674aa4cb049a',
+                siteId: 'f4abf57e-23fa-4f83-ad95-178d6a773bc2',
+                branch: 'main',
+                selector: 'div#search',
+              });
+            `
+          }
+        </Script>
+      </body>
     </html>
   );
 }
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -20,6 +20,7 @@ export default async function BlogLanding() {
   return (
     <main className="container mx-auto px-8 my-10">
       <h1 className="text-3xl font-bold mb-4">Blog Editorial</h1>
+      <div id="search" />
       <section className="grid grid-cols-4 gap-2">
         {posts.map(post => (
           <Link key={post?.id} href={`/posts/${post?.slug}`} className="px-4 py-2 border border-slate-400 rounded-md">
